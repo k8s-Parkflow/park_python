ab76ad16e87aaf446819b3c0639b28749eef20c0
Dobby19 <southvi1@naver.com>
2026-03-10 00:17:00 +0900

    feat: add parking command swagger docs

--- park_py/urls.py
+++ park_py/urls.py
@@ -19,9 +19,12 @@ from django.urls import include, path
 
 from park_py.error_handling import handler404 as json_handler404
 from park_py.error_handling import handler500 as json_handler500
+from park_py.openapi import openapi_json_view, swagger_ui_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/docs/openapi.json", openapi_json_view, name="openapi-json"),
+    path("api/docs/swagger", swagger_ui_view, name="swagger-ui"),
     path("", include("parking_command_service.urls")),
 ]
 
