0890530577a7d78e464f12bcf9c5f2eb03973b12
Dobby19 <southvi1@naver.com>
2026-03-12 15:46:08 +0900

    feat: add orchestration-only http runtime

---
+++ services/orchestration-service/src/orchestration_service/http_runtime/urls.py
@@ -0,0 +1,8 @@
+from orchestration_service.urls import urlpatterns
+
+from park_py.error_handling import handler404 as json_handler404
+from park_py.error_handling import handler500 as json_handler500
+
+
+handler404 = json_handler404
+handler500 = json_handler500
